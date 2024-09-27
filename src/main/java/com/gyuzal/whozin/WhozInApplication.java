031bbf09cf1c83dea537dd52469c29854e31bceb
KimJongMin <whdals011203@naver.com>
2024-09-27 15:53:40 +0900

    rename: 패키지 명 변경

--- src/main/java/com/gyuzal/whozin/WhozInApplication.java
+++ src/main/java/com/gyuzal/whozin/WhozInApplication.java
@@ -1,4 +1,4 @@
-package com.whozin.whozin;
+package com.gyuzal.whozin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gyuzal/whozin/WhozInApplicationTests.java
+++ src/test/java/com/gyuzal/whozin/WhozInApplicationTests.java
@@ -1,4 +1,4 @@
-package com.whozin.whozin;
+package com.gyuzal.whozin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
